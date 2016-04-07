311ea21466475de2e62561e0e5ef4175bd8d6ddd
hallocbr1 <hcps-hallocbr1@henricostudents.org>
2016-04-07 14:48:09 -0400

    Added files via upload

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ class Test{
    //Be sure that you have correct capitalization and spacing
    
    public static void main(String[] args){
-      system.out.println("Hello Wordl!");   
+      System.out.println("Hello World!");
    }
 }
